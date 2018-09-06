45a35ccfd1fd6d699c2a76c8fe7060a2c0303150
Andrey8888 <Andrei.8.8.8@yandex.ru>
2018-09-06 22:16:58 +0300

    OneWayPlatform

---
+++ Assets/Scripts/VerticalPlatform.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class VerticalPlatform : MonoBehaviour
+{
+
+    private PlatformEffector2D effector;
+    public float waitTime;
+
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            waitTime = 0.1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (waitTime <= 0)
+            {
+                effector.rotationalOffset = 180f;
+                waitTime = 0.1f;
+            }
+            else {
+                waitTime -= Time.deltaTime;
+            }
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            effector.rotationalOffset = 0; 
+        }
+    }
+}
