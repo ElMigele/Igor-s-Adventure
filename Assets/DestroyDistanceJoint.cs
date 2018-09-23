7d342ee49d73e3d472feeb5df74977e807355e93
Mikhail <michail.1994@mail.ru>
2018-09-23 13:05:15 +0300

    Revert "Revert "Rammer""
    This reverts commit 519717f58a1d30d6bdb15f2eb638221a7ddaeefe.

---
+++ Assets/DestroyDistanceJoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyDistanceJoint : MonoBehaviour {
+    public DistanceJoint2D HoldingRope;
+
+    public void Destroy()
+    {
+        HoldingRope.enabled = false;
+    }
+}
---
+++ Assets/DestroyHoldingRope.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyHoldingRope : MonoBehaviour {
+    public GameObject Rammer;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.transform.CompareTag("Arrow"))
+        {
+            Destroy(gameObject);
+            Rammer.GetComponent<DestroyDistanceJoint>().Destroy();
+        }
+    }    
+}
